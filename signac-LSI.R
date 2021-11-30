c70f31c55c2d449e148f3d71e14cdae05f0ea4ab
FloWuenne <flowuenne@gmail.com>
2021-11-30 19:24:36 +0000

    First fully functioning version of LSI.

--- signac-LSI.R
+++ signac-LSI.R
@@ -19,6 +19,34 @@ option_list = list(
     help = ""
   ),
   make_option(
+    c("--min-cutoff"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = ""
+  ),
+  make_option(
+    c("--output-depthcor"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "TSS output plot."
+  ),
+  make_option(
+    c("-w", "--png-width"),
+    action = "store",
+    default = 1000,
+    type = 'integer',
+    help = "Width of png (px)."
+  ),
+  make_option(
+    c("-j", "--png-height"),
+    action = "store",
+    default = 1000,
+    type = 'integer',
+    help = "Height of png file (px)."
+  ),
+  make_option(
     c("--output-object-file"),
     action = "store",
     default = NA,
@@ -38,9 +66,13 @@ signac_object <- readRDS(file = opt$signac_object)
 
 
 signac_object <- RunTFIDF(signac_object)
-signac_object <- FindTopFeatures(signac_object, min.cutoff = 'q0')
+signac_object <- FindTopFeatures(signac_object, min.cutoff = opt$min_cutoff)
 signac_object <- RunSVD(signac_object)
 
+## Plot the Depth correlation plot
+png(filename = opt$output_depthcor, width = opt$png_width, height = opt$png_height)
+DepthCor(signac_object)
+dev.off()
 
 # Output to a serialized R object
 saveRDS(signac_object, file = opt$output_object_file)
