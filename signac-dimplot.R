6ed39a0bcc884050e52203ec183d65572d3ecffe
FloWuenne <flowuenne@gmail.com>
2021-12-03 21:14:40 +0000

    Base version of plot function is working.

--- signac-dimplot.R
+++ signac-dimplot.R
@@ -18,77 +18,55 @@ option_list = list(
     type = 'character',
     help = ""
   ),
-make_option(
-    c("-a", "--dim-1"),
+  make_option(
+    c("--dims"),
     action = "store",
-    default = 1,
-    type = 'integer',
+    type = 'character',
     help = "Dimension for x-axis (default 1)"
   ),
-make_option(
-    c("-b", "--dim-2"),
-    action = "store",
-    default = 2,
-    type = 'integer',
-    help = "Dimension for y-axis (default 2)"
-  ),
-make_option(
+  make_option(
     c("-p", "--pt-size"),
     action = "store",
     default = 1,
     type = 'integer',
     help = "Adjust point size for plotting"
   ),
-make_option(
+  make_option(
     c("-l", "--label-size"),
     action = "store",
     default = 4,
     type = 'integer',
     help = "Sets size of labels."
   ),
-make_option(
+  make_option(
     c("-d", "--do-label"),
     action = "store",
     default = FALSE,
     type = 'logical',
     help = "Whether to label the clusters."
   ),
-make_option(
-    c("-f", "--group-by"),
+  make_option(
+    c("--group-by"),
     action = "store",
     default = 'ident',
     type = 'character',
     help = "Group (color) cells in different ways (for example, orig.ident)."
   ),
-make_option(
-    c("-t", "--plot-title"),
-    action = "store",
-    default = 1,
-    type = 'character',
-    help = "Title for plot."
-  ),
-make_option(
-    c("-m", "--do-bare"),
-    action = "store",
-    default = FALSE,
-    type = 'logical',
-    help = "Do only minimal formatting (default : FALSE)"
-  ),
-make_option(
-    c("-w", "--png-width"),
+  make_option(
+    c("--png-width"),
     action = "store",
     default = 1000,
     type = 'integer',
     help = "Width of png (px)."
   ),
-make_option(
-    c("-j", "--png-height"),
+  make_option(
+    c("--png-height"),
     action = "store",
     default = 1000,
     type = 'integer',
     help = "Height of png file (px)."
   ),
-make_option(
+  make_option(
     c("--output_image_file"),
     action = "store",
     default = NA,
@@ -104,9 +82,15 @@ suppressPackageStartupMessages(require(Signac))
 
 set.seed(1234)
 
+dims_use <- opt$dims
+if ( ! is.null(dims_use)){
+  dims_parsed <- wsc_parse_numeric(opt, 'dims')
+  dims_use <- seq(from = dims_parsed[1], to = dims_parsed[2])
+}
+
 # extract gene annotations from EnsDb
 signac_object <- readRDS(file = opt$signac_object)
 
 png(filename = opt$output_image_file, width = opt$png_width, height = opt$png_height)
-DimPlot(object = signac_object, dim.1 = opt$dim_1, dim.2 = opt$dim_2, pt.size = opt$pt_size, label.size = opt$label_size, do.label = opt$do_label, group.by = opt$group_by, do.bare=opt$do_bare) + NoLegend()
+DimPlot(object = signac_object, dims = dims_use, pt.size = opt$pt_size, label.size = opt$label_size, group.by = opt$group_by) + NoLegend()
 dev.off()
