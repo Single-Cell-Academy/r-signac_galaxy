81962dc7811fb72ccefb0a88acbf80a77b1ad42a
JoelPHoward <joel.p.howard@gmail.com>
2021-12-27 20:15:57 +0000

    createAssayObject

---
+++ signac-createAssayObject.R
@@ -0,0 +1,116 @@
+#!/usr/bin/env Rscript
+
+# Load optparse we need to check inputs
+
+suppressPackageStartupMessages(require(optparse))
+
+# Load common functions
+
+suppressPackageStartupMessages(require(workflowscriptscommon))
+
+# parse options
+option_list = list(
+  make_option(
+    c("--signac-object"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "A Seurat object."
+  ),
+  make_option(
+    c("--counts"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "Counts Matrix."
+  ),
+  make_option(
+    c("--data"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "Data Matrix."
+  ),
+  make_option(
+    c("--name"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "Assay Name."
+  ),
+  make_option(
+    c("--min-cells"),
+    action = "store",
+    default = NA,
+    type = 'numeric',
+    help = "Min Cells."
+  ),
+  make_option(
+    c("--min-features"),
+    action = "store",
+    default = NA,
+    type = 'numeric',
+    help = "Min Features."
+  ),
+  make_option(
+    c("--method"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "Method for normalization."
+  ),
+  make_option(
+    c("--scale-factor"),
+    action = "store",
+    default = 10000,
+    type = 'numeric',
+    help = "Sets the scale factor for cell-level normalization."
+  ),
+  make_option(
+    c("--margin"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "If performing CLR normalization, normalize across features (1) or cells (2)."
+  ),
+  make_option(
+    c("--output-object-file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "File name in which to store serialized R matrix object."
+  ) 
+)
+
+opt <- wsc_parse_args(option_list)
+
+suppressPackageStartupMessages(require(Seurat))
+suppressPackageStartupMessages(require(Signac))
+
+set.seed(1234)
+
+if (! file.exists(opt$signac_object)){
+  stop((paste('File', opt$signac_object, 'does not exist')))
+}
+if(!is.null(opt$counts)){
+  if (! file.exists(opt$counts)){
+    stop((paste('File', opt$data, 'does not exist')))
+  }
+}else if(!is.null(opt$data)){
+  if (! file.exists(opt$data)){
+    stop((paste('File', opt$data, 'does not exist')))
+  }
+}else{
+  stop(paste("A 'counts' or 'data' file must be supplied."))
+}
+
+signac_object <- readRDS(file = opt$signac_object)
+
+if(!is.null(opt$counts)){
+    signac_object[[opt$name]] <- CreateAssayObject(counts = read.csv(opt$counts, header = T, row.names = 1), min.cells = opt$min_cells, min.features = opt$min_features)
+}else{
+    signac_object[[opt$name]] <- CreateAssayObject(data = read.csv(opt$data, header = T, row.names = 1), min.cells = opt$min_cells, min.features = opt$min_features)
+}
+signac_object <- NormalizeData(object = signac_object, normalization.method = opt$method, scale.factor = opt$scale_factor, margin = opt$margin, assay = opt$name)
+
+saveRDS(signac_object, file = opt$output_object_file)
